5f0b95c3d685961db72101815f6ff0219ec37666
Goulven <goulven.corbic@gmail.com>
2024-01-23 15:04:55 +0100

    Cross-compilation Backend test GPIO

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,8 @@
-use bb::{
-    gpio::{
-        GPIO,
-        PinDirection
-    },
-    prelude::{
-        DeviceState,
-        PinState
-    }
+use gpio::{
+    GpioIn, GpioOut
 };
-use libbeaglebone as bb;
-
 fn main() {
-    let mut led = GPIO::new(5);
-    led.set_direction(PinDirection::Out).expect("TODO: panic message");
-    led.set_export(DeviceState::Exported).expect("TODO: panic message");
-    led.write(PinState::High).expect("TODO: panic message");
+
     println!("Hello, world!");
 }
 
